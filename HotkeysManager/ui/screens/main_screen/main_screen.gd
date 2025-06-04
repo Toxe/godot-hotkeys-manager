3c270bbdace90aa34a6eecb593bccc44eb1a55dd
Toxe <toxe@toxe.net>
2025-06-04 18:58:03 +0200

    Show program groups on the main screen

--- HotkeysManager/ui/screens/main_screen/main_screen.gd
+++ HotkeysManager/ui/screens/main_screen/main_screen.gd
@@ -1,15 +1,26 @@
 class_name MainScreen extends Control
 
+const programgroup_scene: PackedScene = preload("uid://ctfylvylgevof")
+
 var _db: Database = null
 
 
 func _ready() -> void:
     assert(_db.is_open())
 
+    var res := _db.query("SELECT id, name FROM programgroup ORDER BY name")
+    if res.ok:
+        for row: Dictionary in res.rows:
+            add_programgroup(row)
+
 
 func setup(db: Database) -> void:
     _db = db
 
 
-func _on_button_pressed() -> void:
-    Events.switch_to_commands_screen.emit()
+func add_programgroup(row: Dictionary) -> void:
+    var id: int = row.id
+    var programgroup_name: String = row.name
+    var programgroup := programgroup_scene.instantiate() as Programgroup
+    programgroup.setup(_db, id, programgroup_name)
+    $VBoxContainer/ProgramgroupList.add_child(programgroup)
---
+++ HotkeysManager/ui/screens/main_screen/ui/programgroup.gd
@@ -0,0 +1,40 @@
+class_name Programgroup extends Control
+
+var _db: Database = null
+var _programgroup_id: int = -1
+var _programgroup_name: String = ""
+
+@onready var item_list: ItemList = $HBoxContainer/ItemList
+
+
+func _ready() -> void:
+    ($TitleLabel as Label).text = _programgroup_name
+
+    var sql := "SELECT pp.programgroup_id, pp.program_id, p.name
+FROM programgroup_program pp
+INNER JOIN program p ON pp.program_id = p.id
+WHERE pp.programgroup_id = ?
+ORDER BY p.name"
+
+    var res := _db.query(sql, [_programgroup_id])
+    if res.ok:
+        for row: Dictionary in res.rows:
+            add_program(row)
+
+
+func setup(db: Database, id: int, programgroup_name: String) -> void:
+    _db = db
+    _programgroup_id = id
+    _programgroup_name = programgroup_name
+
+
+func add_program(row: Dictionary) -> void:
+    var program_name: String = row.name
+    var program_id: int = row.program_id
+    var index := item_list.add_item(program_name)
+    item_list.set_item_metadata(index, program_id)
+
+
+func _on_item_list_item_selected(index: int) -> void:
+    var program_id: int = item_list.get_item_metadata(index)
+    prints(index, program_id)
