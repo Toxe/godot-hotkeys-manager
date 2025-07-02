cbe218386b02a269ef398487ba0ae6c63b4b386c
Toxe <toxe@toxe.net>
2025-07-02 18:47:55 +0200

    Add Database tests

---
+++ HotkeysManager/tests/test_database.gd
@@ -0,0 +1,91 @@
+extends GutTest
+
+var db: Database = null
+
+
+func before_each() -> void:
+    db = Database.new()
+    db.open(":memory:")
+
+
+func after_each() -> void:
+    db.close()
+    db = null
+
+
+func test_insert_rows() -> void:
+    assert_true(db.insert_rows("program", [ {"name": "test1"}, {"name": "test2"}]))
+    assert_eq(db.last_insert_rowid(), 9)
+
+
+func test_insert_row() -> void:
+    assert_true(db.insert_row("program", {"name": "test"}))
+    assert_eq(db.last_insert_rowid(), 8)
+
+
+func test_update_rows() -> void:
+    assert_true(db.update_rows("program_command", "name='Go to File'", {"name": "test"}))
+
+
+func test_delete_rows() -> void:
+    assert_true(db.delete_rows("program_command", "name='Go to File'"))
+
+
+func test_select_rows() -> void:
+    var rows: Variant = db.select_rows("program_command", "name='Go to File'", ["program_id", "command_id", "name"])
+    assert_eq_deep(rows, [
+        {"program_id": 1, "command_id": 1, "name": "Go to File"},
+        {"program_id": 3, "command_id": 1, "name": "Go to File"},
+    ])
+
+    # return false on database error
+    assert_false(db.select_rows("missing_table", "id=99", ["col1", "col2"]))
+
+
+func test_select_row() -> void:
+    var row: Variant = db.select_row("user_hotkey", "hotkey='Ctrl+P'", ["command_id", "hotkey"])
+    assert_eq_deep(row, {"command_id": 1, "hotkey": "Ctrl+P"})
+
+    # return false on database error
+    assert_false(db.select_row("missing_table", "id=99", ["col1", "col2"]))
+
+    # return false if there is more than one result row
+    assert_false(db.select_row("program_command", "program_id=1", ["name"]))
+
+    # return null if the row doesn't exist
+    assert_null(db.select_row("program_command", "program_id=99", ["id", "name"]))
+
+
+func test_select_value() -> void:
+    var value: Variant = db.select_value("user_hotkey", "command_id=2", "hotkey")
+    assert_eq(value, "Ctrl+PageDown")
+
+    # return false on database error
+    assert_false(db.select_value("missing_table", "id=99", "col"))
+
+    # return false if there is more than one result row
+    assert_false(db.select_value("program_command", "program_id=1", "name"))
+
+    # return null if the row doesn't exist
+    assert_null(db.select_value("program_command", "program_id=99", "name"))
+
+
+func test_select() -> void:
+    # without bindings
+    var rows1: Variant = db.select("SELECT program_id, command_id, name FROM program_command WHERE name='Go to File';")
+    assert_eq_deep(rows1, [
+        {"program_id": 1, "command_id": 1, "name": "Go to File"},
+        {"program_id": 3, "command_id": 1, "name": "Go to File"},
+    ])
+
+    # with bindings
+    var rows2: Variant = db.select("SELECT program_id, command_id, name FROM program_command WHERE name=?;", ["Go to File"])
+    assert_eq_deep(rows2, [
+        {"program_id": 1, "command_id": 1, "name": "Go to File"},
+        {"program_id": 3, "command_id": 1, "name": "Go to File"},
+    ])
+
+
+func test_query() -> void:
+    var sql := "CREATE TABLE `foo` (`id` integer PRIMARY KEY NOT NULL, `name` varchar(255)); INSERT INTO `foo` (`name`) VALUES ('first'), ('second');"
+    assert_true(db.query(sql))
