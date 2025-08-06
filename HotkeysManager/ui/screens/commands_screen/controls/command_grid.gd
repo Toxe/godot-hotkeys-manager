71b5009fd073229e42ed6d560e75c40745f37a61
Toxe <toxe@toxe.net>
2025-08-06 20:28:44 +0200

    Code cleanup

--- HotkeysManager/ui/screens/commands_screen/controls/command_grid.gd
+++ HotkeysManager/ui/screens/commands_screen/controls/command_grid.gd
@@ -35,27 +35,13 @@ func add_header_row(programs: Dictionary[int, String], program_abbreviations: Di
 
 
 func add_command_row(command_id: int, programs: Dictionary[int, String], commands: Dictionary[int, String], program_commands: Dictionary[int, Dictionary], program_command_hotkeys: Dictionary[int, Dictionary], user_hotkeys: Dictionary[int, Dictionary], user_hotkey_programs: Dictionary[int, Dictionary]) -> void:
-    var command_name: String = commands[command_id]
-    var command_button := add_command_button(command_name)
-    command_button.pressed.connect(_on_rename_command_button_pressed.bind(command_name, command_id))
+    add_command_button(command_id, commands[command_id])
 
     for program_id: int in programs:
         add_program_hotkeys_control(command_id, program_id, program_commands, program_command_hotkeys)
 
     add_user_hotkey_control(command_id, user_hotkeys)
-
-    if command_id in user_hotkeys:
-        var user_hotkey_id: int = user_hotkeys[command_id]["user_hotkey_id"]
-        var hotkeys: Array = user_hotkey_programs[command_id].get("hotkeys") if command_id in user_hotkey_programs else []
-
-        for program_id in programs:
-            var s := "✔️" if program_id in hotkeys else "❌"
-            var label := add_label(s)
-            label.mouse_filter = Control.MOUSE_FILTER_PASS
-            label.gui_input.connect(_on_user_hotkey_program_checkbox_gui_input.bind(user_hotkey_id, program_id, label))
-    else:
-        for program_id in programs:
-            add_label("–")
+    add_user_hotkey_program_controls(command_id, programs, user_hotkeys, user_hotkey_programs)
 
 
 func add_label(text: String) -> Label:
@@ -95,12 +81,13 @@ func add_header_program_abbreviation(program_name: String, program_abbr: String)
     return label
 
 
-func add_command_button(text: String) -> Button:
+func add_command_button(command_id: int, command_name: String) -> Button:
     var button := Button.new()
-    button.text = text
+    button.text = command_name
     button.theme_type_variation = "CommandButton"
     button.alignment = HORIZONTAL_ALIGNMENT_LEFT
     button.size_flags_vertical = Control.SIZE_SHRINK_BEGIN
+    button.pressed.connect(_on_rename_command_button_pressed.bind(command_name, command_id))
     add_child(button)
     return button
 
@@ -117,6 +104,21 @@ func add_user_hotkey_control(command_id: int, user_hotkeys: Dictionary[int, Dict
     control.setup(_db, _programgroup_id, command_id, user_hotkeys)
     add_child(control)
     return control
+
+
+func add_user_hotkey_program_controls(command_id: int, programs: Dictionary[int, String], user_hotkeys: Dictionary[int, Dictionary], user_hotkey_programs: Dictionary[int, Dictionary]) -> void:
+    if command_id in user_hotkeys:
+        var user_hotkey_id: int = user_hotkeys[command_id]["user_hotkey_id"]
+        var hotkeys: Array = user_hotkey_programs[command_id].get("hotkeys") if command_id in user_hotkey_programs else []
+
+        for program_id in programs:
+            var s := "✔️" if program_id in hotkeys else "❌"
+            var label := add_label(s)
+            label.mouse_filter = Control.MOUSE_FILTER_PASS
+            label.gui_input.connect(_on_user_hotkey_program_checkbox_gui_input.bind(user_hotkey_id, program_id, label))
+    else:
+        for program_id in programs:
+            add_label("–")
 
 
 func _on_rename_command_button_pressed(command_name: String, command_id: int) -> void:
