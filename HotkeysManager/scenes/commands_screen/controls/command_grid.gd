819228357dbb742ef38a84ca1969279f8db3cf0e
Toxe <toxe@toxe.net>
2025-10-21 12:08:35 +0200

    Clicking a checkbox to change a user hotkey program assignment doesn't reload the screen anymore

--- HotkeysManager/scenes/commands_screen/controls/command_grid.gd
+++ HotkeysManager/scenes/commands_screen/controls/command_grid.gd
@@ -147,8 +147,8 @@ func add_user_hotkey_program_controls(command_id: int, programs: Dictionary[int,
             if command_id in user_hotkeys:
                 var user_hotkey_id: int = user_hotkeys[command_id]["user_hotkey_id"]
                 var hotkeys: Array = user_hotkey_programs[command_id].get("hotkeys") if command_id in user_hotkey_programs else []
-                checkbox.button_pressed = program_id in hotkeys
-                checkbox.toggled.connect(_on_user_hotkey_program_checkbox_toggled.bind(user_hotkey_id, program_id))
+                checkbox.set_pressed_no_signal(program_id in hotkeys)
+                checkbox.toggled.connect(_on_user_hotkey_program_checkbox_toggled.bind(checkbox, user_hotkey_id, program_id))
             else:
                 checkbox.disabled = true
 
@@ -194,12 +194,14 @@ func bind_program_command_cells(program_id: int, command_id: int) -> void:
             cell.is_bound = true
 
 
-func _on_user_hotkey_program_checkbox_toggled(toggled_on: bool, user_hotkey_id: int, program_id: int) -> void:
+func _on_user_hotkey_program_checkbox_toggled(toggled_on: bool, checkbox: CheckBox, user_hotkey_id: int, program_id: int) -> void:
+    var success := true
     if toggled_on:
-        _db.insert_row("user_hotkey_program", {"user_hotkey_id": user_hotkey_id, "program_id": program_id})
+        success = _db.insert_row("user_hotkey_program", {"user_hotkey_id": user_hotkey_id, "program_id": program_id})
     else:
-        _db.delete_rows("user_hotkey_program", "user_hotkey_id=%d AND program_id=%d" % [user_hotkey_id, program_id])
-    Events.switch_to_commands_screen.emit.call_deferred(_programgroup_id)
+        success = _db.delete_rows("user_hotkey_program", "user_hotkey_id=%d AND program_id=%d" % [user_hotkey_id, program_id])
+    if !success:
+        checkbox.set_pressed_no_signal(!toggled_on)
 
 
 func _on_command_name_cell_changed(cell: TextCell, old_name: String, new_name: String, command_id: int) -> void:
