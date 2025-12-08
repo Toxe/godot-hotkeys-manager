6afe0eded3d1c8c3f413dd44c43708d646930c46
Toxe <toxe@toxe.net>
2025-12-08 10:29:34 +0100

    Readability

--- HotkeysManager/db/schema.sql
+++ HotkeysManager/db/schema.sql
@@ -1,75 +1,75 @@
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE properties (
-    version integer
+    version INTEGER
 );
 
 CREATE TABLE programgroup (
-    programgroup_id integer PRIMARY KEY NOT NULL,
-    name varchar(255) COLLATE nocase
+    programgroup_id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT COLLATE nocase
 );
 
 CREATE TABLE program (
-    program_id integer PRIMARY KEY NOT NULL,
-    name varchar(255) COLLATE nocase,
-    abbreviation varchar(16) COLLATE nocase,
-    icon blob
+    program_id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT COLLATE nocase,
+    abbreviation TEXT COLLATE nocase,
+    icon BLOB
 );
 
 CREATE TABLE programgroup_program (
-    programgroup_id integer NOT NULL,
-    program_id integer NOT NULL,
+    programgroup_id INTEGER NOT NULL,
+    program_id INTEGER NOT NULL,
     PRIMARY KEY (programgroup_id, program_id),
     FOREIGN KEY (programgroup_id) REFERENCES programgroup (programgroup_id) ON DELETE CASCADE,
     FOREIGN KEY (program_id) REFERENCES program (program_id) ON DELETE CASCADE
 );
 
 CREATE TABLE category (
-    category_id integer PRIMARY KEY NOT NULL,
-    name varchar(255) COLLATE nocase
+    category_id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT COLLATE nocase
 );
 
 CREATE TABLE command (
-    command_id integer PRIMARY KEY NOT NULL,
-    name varchar(255) COLLATE nocase
+    command_id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT COLLATE nocase
 );
 
 CREATE TABLE command_category (
-    command_id integer NOT NULL,
-    category_id integer NOT NULL,
+    command_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
     PRIMARY KEY (command_id, category_id),
     FOREIGN KEY (command_id) REFERENCES command (command_id) ON DELETE CASCADE,
     FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE CASCADE
 ) WITHOUT ROWID;
 
 CREATE TABLE program_command_hotkey (
-    program_id integer NOT NULL,
-    command_id integer NOT NULL,
-    hotkey varchar(64) NOT NULL,
+    program_id INTEGER NOT NULL,
+    command_id INTEGER NOT NULL,
+    hotkey TEXT NOT NULL,
     PRIMARY KEY (program_id, command_id, hotkey),
     FOREIGN KEY (program_id) REFERENCES program (program_id) ON DELETE CASCADE,
     FOREIGN KEY (command_id) REFERENCES command (command_id) ON DELETE CASCADE
 );
 
 CREATE TABLE comment (
-    comment_id integer PRIMARY KEY NOT NULL,
-    comment_text text,
-    time_created datetime DEFAULT (datetime()),
-    time_changed datetime DEFAULT (datetime())
+    comment_id INTEGER PRIMARY KEY NOT NULL,
+    comment_text TEXT,
+    time_created DATETIME DEFAULT (datetime()),
+    time_changed DATETIME DEFAULT (datetime())
 );
 
 CREATE TABLE command_comment (
-    command_id integer NOT NULL,
-    comment_id integer NOT NULL,
+    command_id INTEGER NOT NULL,
+    comment_id INTEGER NOT NULL,
     PRIMARY KEY (command_id, comment_id),
     FOREIGN KEY (command_id) REFERENCES command (command_id) ON DELETE CASCADE,
     FOREIGN KEY (comment_id) REFERENCES comment (comment_id) ON DELETE CASCADE
 ) WITHOUT ROWID;
 
 CREATE TABLE program_command_comment (
-    program_id integer NOT NULL,
-    command_id integer NOT NULL,
-    comment_id integer NOT NULL,
+    program_id INTEGER NOT NULL,
+    command_id INTEGER NOT NULL,
+    comment_id INTEGER NOT NULL,
     PRIMARY KEY (program_id, command_id, comment_id),
     FOREIGN KEY (program_id) REFERENCES program (program_id) ON DELETE CASCADE,
     FOREIGN KEY (command_id) REFERENCES command (command_id) ON DELETE CASCADE,
@@ -77,23 +77,23 @@ CREATE TABLE program_command_comment (
 ) WITHOUT ROWID;
 
 CREATE TABLE user_hotkey (
-    user_hotkey_id integer PRIMARY KEY NOT NULL,
-    command_id integer NOT NULL,
-    hotkey varchar(64),
+    user_hotkey_id INTEGER PRIMARY KEY NOT NULL,
+    command_id INTEGER NOT NULL,
+    hotkey TEXT,
     FOREIGN KEY (command_id) REFERENCES command (command_id) ON DELETE CASCADE
 );
 
 CREATE TABLE user_hotkey_program (
-    user_hotkey_id integer NOT NULL,
-    program_id integer NOT NULL,
+    user_hotkey_id INTEGER NOT NULL,
+    program_id INTEGER NOT NULL,
     PRIMARY KEY (user_hotkey_id, program_id),
     FOREIGN KEY (user_hotkey_id) REFERENCES user_hotkey (user_hotkey_id) ON DELETE CASCADE,
     FOREIGN KEY (program_id) REFERENCES program (program_id) ON DELETE CASCADE
 ) WITHOUT ROWID;
 
 CREATE TABLE user_hotkey_comment (
-    user_hotkey_id integer NOT NULL,
-    comment_id integer NOT NULL,
+    user_hotkey_id INTEGER NOT NULL,
+    comment_id INTEGER NOT NULL,
     PRIMARY KEY (user_hotkey_id, comment_id),
     FOREIGN KEY (user_hotkey_id) REFERENCES user_hotkey (user_hotkey_id) ON DELETE CASCADE,
     FOREIGN KEY (comment_id) REFERENCES comment (comment_id) ON DELETE CASCADE
